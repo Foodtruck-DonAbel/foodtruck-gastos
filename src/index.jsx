0e76d9fd26ca6422aca63e66c19a0fcb4cc2a93c
Foodtruck-DonAbel <a.monsalves1981@gmail.com>
2026-08-08 00:01:35 -0400

    index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import Menu from './Menu.jsx';
+import Pedidos from './Pedidos.jsx';
 
-const isMenu = window.location.pathname === '/menu';
+const path = window.location.pathname;
+const Component = path === '/menu' ? Menu : path === '/pedidos' ? Pedidos : App;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {isMenu ? <Menu /> : <App />}
-  </React.StrictMode>
-);
+root.render(<React.StrictMode><Component /></React.StrictMode>);
